71fadfdea48c1ced178890288716c869d32c3d39
Ilya Durdyev <funbanji@gmail.com>
2018-02-02 19:09:30 +0300

    Added hostname filter

--- src/main/java/ru/torgcrm/ecommerce/shop/controllers/IndexController.java
+++ src/main/java/ru/torgcrm/ecommerce/shop/controllers/IndexController.java
@@ -3,6 +3,7 @@ package ru.torgcrm.ecommerce.shop.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.torgcrm.ecommerce.shop.models.Item;
 import ru.torgcrm.ecommerce.shop.repository.ItemRepository;
@@ -16,10 +17,11 @@ public class IndexController {
     ItemRepository itemRepository;
 
     @RequestMapping("/")
-    public String index(Model model) {
+    public String index(@RequestAttribute("domain") String domain, Model model) {
         List<Item> items = itemRepository.findAll();
 
         model.addAttribute("items", items);
+        model.addAttribute("domain", domain);
         return "index";
     }
 }
