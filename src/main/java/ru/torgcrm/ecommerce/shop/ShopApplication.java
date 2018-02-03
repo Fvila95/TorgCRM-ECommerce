89142bca6abd69b522ea5a5168b15d4066be1747
Ilya Durdyev <funbanji@gmail.com>
2018-02-03 19:57:47 +0300

    build war files

--- src/main/java/ru/torgcrm/ecommerce/shop/ShopApplication.java
+++ src/main/java/ru/torgcrm/ecommerce/shop/ShopApplication.java
@@ -3,10 +3,17 @@ package ru.torgcrm.ecommerce.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class ShopApplication {
+public class ShopApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ShopApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
